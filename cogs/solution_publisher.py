dbcf87f6f52c00137532e90dbd81dbbbe03e701d
Tabris <afiery1yt@gmail.com>
2022-09-18 17:51:06 -0700

    refactored to use a websocket rather than a recurring get request

--- cogs/solution_publisher.py
+++ cogs/solution_publisher.py
@@ -1,40 +1,42 @@
 from collections import namedtuple
-from datetime import datetime
 from discord.ext.commands import Cog
-from discord.ext.tasks import loop
 from discord import NotFound
 from requests import get
 
-# a cog that handles grabbing solution data from the website and posting it to the solutions channel
+# a cog that publishes newly posted solution data from the websocket listener to the solutions text channel
 class SolutionPublisher(Cog):
     def __init__(self, client):
-        self.client = client # pointer to the bot object
+        self.client = client # reference to the bot object
         self.channel = None  # which channel to send the solution messages to
-        self.last_called = datetime.utcnow().isoformat() # time elapsed since the last get request
         self.Solution = namedtuple("solution", "username problem_title problem_id code") # tuple containing all relevent parsed solution data
-
-    # cleanup
-    def cog_unload(self):
-        self.publish_solutions.cancel()
-
-    # async function that queries the website's api for any successful solutions submitted since the last iteration of the event loop
-    async def parse_get(self):
-        data = get('https://api.meters.sh/submissions/new-completions', params = {'since': self.last_called}).json()
-        self.last_called = datetime.utcnow().isoformat()
-        for item in data:
-            problem_data = get(f"https://api.meters.sh/problems/{item['problem_id']}").json()
-            user_data = get(f"https://api.meters.sh/user/id/{item['user_id']}").json()
-
-            # it is possible, though unlikely, that the submitter is not a member of the discord server, in which case we default to their website username
-            username = user_data['username']
-            try:
-                username = (await self.client.fetch_user(int(user_data['discord_id']))).mention
-            except NotFound as e:
-                print(f"WARN: function 'parse_get' raised: {e}\n(this probably just means someone submitted a succesful solution who is not in the discord server)")
-            yield self.Solution(username, problem_data['title'], problem_data['id'], item['code'])
     
-    # async function that takes a parsed solution and sends a message in the solutions channel with the username of the submitter and the name of the problem they solved
-    # followed by as many messages of code as are required to send the whole solution given Discord's 2000 character message limit
+    # find the solutions channel
+    @Cog.listener()
+    async def on_ready(self):
+        for channel in self.client.guilds[0].text_channels:
+            if(channel.name == 'solutions'):
+                self.channel = channel
+                break
+
+    # parses solution json data and publishes it to the solutions channel
+    async def publish_solution(self, solution):
+        parsed_solution = await self.parse_solution(solution)
+        await self.print_solution(parsed_solution)
+
+    # extracts relevent information from solution json data and returns it as a tuple
+    async def parse_solution(self, solution):
+        problem_data = get(f"https://api.meters.sh/problems/{solution['problem_id']}").json()
+        user_data = get(f"https://api.meters.sh/user/id/{solution['user_id']}").json()
+
+        # it is possible, though unlikely, that the submitter is not a member of the discord server, in which case we default to their website username
+        username = user_data['username']
+        try:
+            username = (await self.client.fetch_user(int(user_data['discord_id']))).mention
+        except NotFound as e:
+            print(f"WARN: function 'parse_get' raised: {e}\n(this probably just means someone submitted a succesful solution who is not in the discord server)")
+        return self.Solution(username, problem_data['title'], problem_data['id'], solution['code'])
+    
+    # sends a message containing parsed solution data in the solutions channel
     async def print_solution(self, solution):
         await self.channel.send(f"{solution.username} has successfully completed {solution.problem_title} (https://chicoacm.org/problems/{solution.problem_id}) with the following solution:")
         solution_code = solution.code.split('\n') # the translate sanatizes any triple backticks in the original code which would mess up the code formatting
@@ -56,21 +58,6 @@ class SolutionPublisher(Cog):
             code_messages.append(f"{cur_message}```")
         for message in code_messages:
             await self.channel.send(message)
-
-    # async function that gets all the successful solutions since it was last called and submits each to self.channel, called once every three seconds
-    @loop(seconds=3.0)
-    async def publish_solutions(self):
-            async for solution in self.parse_get():
-                await self.print_solution(solution)
-
-    # find the solutions channel and start the publish_solutions coroutine
-    @Cog.listener()
-    async def on_ready(self):
-        for channel in self.client.guilds[0].text_channels:
-            if(channel.name == 'solutions'):
-                self.channel = channel
-                break
-        self.publish_solutions.start()
 
 # loads the cog
 def setup(client):
---
+++ cogs/ws_listener.py
@@ -0,0 +1,31 @@
+from discord.ext.commands import Cog
+from discord.ext.tasks import loop
+from websockets import connect
+from json import loads
+
+# a cog that manages a websocket listener and passes off the data it recieves to other cogs for processing
+class WebsocketListener(Cog):
+    def __init__(self, client):
+        self.client = client # reference to the bot object
+    
+    # adds the websocket listener to the Discord event loop
+    @Cog.listener()
+    async def on_ready(self):
+        self.listen.start()
+    
+    # listens for incoming data from the websocket and hands it off to the appropriate cog
+    @loop()
+    async def listen(self):
+        async with connect('ws://api.meters.sh/ws') as ws:
+             for key, value in loads(await ws.recv()).items():
+                if(key == "NewCompletion"):
+                    await self.client.get_cog("SolutionPublisher").publish_solution(value)
+
+    # cleanup
+    def cog_unload(self):
+        self.listen.cancel()
+
+
+# loads the cog
+def setup(client):
+    client.add_cog(WebsocketListener(client))
