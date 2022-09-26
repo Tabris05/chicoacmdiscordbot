0e787f53fbaefd977b57fac79cc0f4fcf76a7f8d
Tabris <afiery1yt@gmail.com>
2022-09-26 01:50:04 -0700

    removed a try/except block made unnecessary by an update to the websocket server

--- cogs/ws_listener.py
+++ cogs/ws_listener.py
@@ -1,6 +1,6 @@
 from discord.ext.commands import Cog
 from discord.ext.tasks import loop
-from websockets import connect, ConnectionClosed
+from websockets import connect
 from json import loads
 
 # a cog that manages a websocket listener and passes off the data it recieves to other cogs for processing
@@ -16,15 +16,12 @@ class WebsocketListener(Cog):
     # listens for incoming data from the websocket and hands it off to the appropriate cog
     @loop()
     async def listen(self):
-        try:
-            async with connect('wss://api.meters.sh/ws') as ws:
-                for key, value in loads(await ws.recv()).items():
-                    if(key == "NewCompletion" or key == "NewStar"):
-                        await self.client.get_cog("SolutionPublisher").publish_solution(value)
-                    elif(key == "NewProblem"):
-                        await self.client.get_cog("ProblemPublisher").publish_problem(value)
-        except ConnectionClosed as e:
-            print(f"WARN: function 'listen' raised: {e}\n(this means the connection to the websocket server is unstable)")
+        async with connect('wss://api.meters.sh/ws') as ws:
+            for key, value in loads(await ws.recv()).items():
+                if(key == "NewCompletion" or key == "NewStar"):
+                    await self.client.get_cog("SolutionPublisher").publish_solution(value)
+                elif(key == "NewProblem"):
+                    await self.client.get_cog("ProblemPublisher").publish_problem(value)
 
     # cleanup
     def cog_unload(self):
