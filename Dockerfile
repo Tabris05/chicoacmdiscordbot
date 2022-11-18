57bd665991977f44a27422c4e002602d2fa38033
Tabris <afiery1yt@gmail.com>
2022-11-17 20:51:57 -0800

    removed local test that accidentally snuck by and added cookie authentication to the websocket connection

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-FROM python:3.9-slim-bullseye
+FROM python:3.11-slim-bullseye
 
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
 RUN apt-get -y update
-RUN apt-get -y install git
-RUN pip3 install -r requirements.txt
+RUN apt-get -y install gcc
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
 
 COPY . .
 
--- cogs/4_ws_listener.py
+++ cogs/4_ws_listener.py
@@ -2,6 +2,7 @@ from discord.ext.commands import Cog
 from discord.ext.tasks import loop
 from websockets import connect
 from json import loads
+from os import getenv
 
 # a cog that manages a websocket listener and passes off the data it recieves to other cogs for processing
 class WebsocketListener(Cog):
@@ -11,14 +12,12 @@ class WebsocketListener(Cog):
     # adds the websocket listener to the Discord event loop
     @Cog.listener()
     async def on_ready(self):
-        value = {'id': 549, 'problem_id': 1, 'user_id': 1, 'success': True, 'runtime': 5020158, 'error': None, 'time': '2022-10-21T09:28:13', 'code': '#include <iostream>\n\nusing namespace std;\n\nvoid fizzBuzz(int n) {\n  for (int i = 1; i <= n; i++) {\n    if (i % 3 == 0&& i % 5 == 0) {\n      cout << "FizzBuzz" << endl;\n    } else if (i % 3 == 0) {\n      cout << "Fizz" << endl;\n    }else if (i % 5 == 0) {\n      cout << "Buzz" << endl;\n    } else {\n      cout << i << endl;\n    }\n  }\n}'}
-        await self.client.get_cog("SolutionPublisher").publish_solution(value)
         self.listen.start()
     
     # listens for incoming data from the websocket and hands it off to the appropriate cog
     @loop()
     async def listen(self):
-        async with connect('wss://api.chicoacm.org/ws') as ws:
+        async with connect('wss://api.chicoacm.org/ws', extra_headers = {'Cookie': f"token={getenv('ACMBOT_COOKIE_TOKEN')}"}) as ws:
             for key, value in loads(await ws.recv()).items():
                 if(key == "NewCompletion" or key == "NewStar"):
                     await self.client.get_cog("SolutionPublisher").publish_solution(value)
